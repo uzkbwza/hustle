e4f8b0eb5d7b77519b9fbd9fbe39f4bd078a94fc
Ian (Ivy) Sly <43023911+uzkbwza@users.noreply.github.com>
2023-01-01 21:19:13 -0800

    0.8.9-unstable

--- characters/swordandgun/states/Impale.gd
+++ characters/swordandgun/states/Impale.gd
@@ -5,41 +5,58 @@ var tp_pos_x = 0
 var tp_pos_y = 0
 var tp_vel_x = "0"
 var tp_vel_y = "0"
+const EXTRA_FRAME_PER_PIXEL = "0.05"
+
+const MIN_LAG_DIST = "200"
 
 const DIST = 16
 
+var lag_frames = 0
+var dir = 1
+
 export var teleport = false
+
 
 func _frame_0():
 	host.z_index = -2
+	lag_frames = 0
 	hit = false
 
-func _frame_6():
+func _frame_5():
 	if teleport:
-		host.start_invulnerability()
+#		host.start_invulnerability()
 		var opp_pos = host.opponent.get_pos()
+		var opp_pos_local = host.obj_local_center(host.opponent)
+		var distance = fixed.vec_len(str(opp_pos_local.x), str(opp_pos_local.y))
+		if fixed.gt(distance, MIN_LAG_DIST):
+			lag_frames = fixed.round(fixed.mul(EXTRA_FRAME_PER_PIXEL, fixed.sub(distance, MIN_LAG_DIST)))
 		host.colliding_with_opponent = false
 		var opp_vel = host.opponent.get_vel()
 		tp_pos_x = opp_pos.x + DIST * host.get_opponent_dir()
+		dir = host.get_opponent_dir()
 		tp_pos_y = opp_pos.y
 		tp_vel_x = opp_vel.x
 		tp_vel_y = opp_vel.y
 
-func _frame_7():
+func _frame_6():
 	if teleport:
-		host.end_invulnerability()
+#		host.end_invulnerability()
 		host.set_vel(tp_vel_x, tp_vel_y)
 		host.set_pos(tp_pos_x, tp_pos_y)
 
-func _frame_8():
+func _frame_7():
 	if teleport:
-		host.update_facing()
+		host.set_facing(dir * -1)
 
 func _tick():
 	host.apply_fric()
 	host.apply_forces()
 	if !(teleport and hit):
 		host.apply_grav()
+	if current_tick >= 8:
+		if lag_frames > 0:
+			lag_frames -= 1
+			current_tick = 8
 	elif current_tick > 30:
 		host.apply_grav()
 		
