5508d7002b49c6a246ecbdb2579d662d8923ea9e
Ian (Ivy) Sly <43023911+uzkbwza@users.noreply.github.com>
2023-02-16 21:36:44 -0800

    1.2.14

--- characters/robo/projectiles/states/OrbitalStrikeFire.gd
+++ characters/robo/projectiles/states/OrbitalStrikeFire.gd
@@ -8,7 +8,7 @@ func _frame_0():
 		host.creator.loic_meter = 0
 		host.creator.can_loic = false
 		host.creator.loic_draining = false
-		print(Utils.int_abs(host.obj_local_center(host.creator).x) <= 20)
+#		print(Utils.int_abs(host.obj_local_center(host.creator).x) <= 20)
 		if Utils.int_abs(host.obj_local_center(host.creator).x) <= 20:
 			host.creator.add_armor_pip()
 			
