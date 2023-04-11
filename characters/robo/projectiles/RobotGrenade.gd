237971ea0f92a687b23c53931bed77e34c441f7a
Ian (Ivy) Sly <43023911+uzkbwza@users.noreply.github.com>
2023-04-11 16:58:57 -0700

    1.4.6-unstable

--- characters/robo/projectiles/RobotGrenade.gd
+++ characters/robo/projectiles/RobotGrenade.gd
@@ -5,14 +5,17 @@ const ACTIVATE_TIME = 30
 const EXPLOSION = preload("res://characters/robo/projectiles/NadeExplosion.tscn")
 const DI_INFLUENCE = "5"
 const DI_HORIZONTAL_MODIFIER = "0.85"
+const DI_DEGRADATION_PER_HIT = "0.5"
 const NUDGE_DISTANCE = 10
-const ARM_TIME_REDUCTION_ON_HIT = 10
+const ARM_TIME_REDUCTION_ON_HIT = 5
 
 onready var my_hitbox = $StateMachine/Active/Hitbox
 onready var active_indicator = $Flip/ActiveIndicator
 
 var last_vel_x
 var last_vel_y
+var hits_chained = 0
+var last_hit_by = -1
 
 var active = false
 
@@ -21,6 +24,9 @@ var ticks_left = LIFETIME
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func init(pos=null):
+	.init(pos)
 
 func tick():
 	.tick()
@@ -55,7 +61,7 @@ func hit_by(hitbox):
 			set_vel(fixed.mul(vel.x, "-1"), vel.y)
 		else:
 			reset_momentum()
-			var dir = fixed.normalized_vec_times(get_hitbox_x_dir(hitbox), hitbox.dir_y, fixed.mul(hitbox.knockback, "1.3"))
+			var dir = fixed.normalized_vec_times(get_hitbox_x_dir(hitbox), hitbox.dir_y, fixed.mul(hitbox.knockback, "1.5"))
 			if is_grounded() and fixed.gt(dir.y, "0"):
 				dir.y = fixed.mul(dir.y, "-1")
 			change_state("Active")
@@ -67,11 +73,28 @@ func hit_by(hitbox):
 				my_hitbox.hit_objects.append(host)
 			var host_object = obj_from_name(host)
 			if host_object:
-				var player = host_object.get_owner().obj_name
+				var player_object = host_object.get_owner()
+				var player = player_object.obj_name
+				hit_cancel_on_hit = true
+				if last_hit_by != host_object.id or player_object.combo_count > 0 or player_object.opponent.combo_count > 0:
+					last_hit_by = host_object.id
+					hits_chained = 0
+				else:
+					hits_chained += 1
+				if hits_chained > 1:
+					hit_cancel_on_hit = false
+				
+				if player_object.combo_count > 0:
+					hit_cancel_on_hit = true
+				
 				if host != player:
 					my_hitbox.hit_objects.append(player)
 				else:
-					var di_force = xy_to_dir(host_object.current_di.x, host_object.current_di.y, DI_INFLUENCE)
+					var di_amount = fixed.mul(fixed.sub("1.0", fixed.mul(DI_DEGRADATION_PER_HIT, str(hits_chained))), DI_INFLUENCE)
+					if fixed.lt(di_amount, "0"):
+						di_amount = "0"
+#					print(di_amount)
+					var di_force = xy_to_dir(host_object.current_di.x, host_object.current_di.y, di_amount)
 					apply_force(fixed.mul(di_force.x, DI_HORIZONTAL_MODIFIER), di_force.y)
 				
 				if active:
@@ -79,6 +102,8 @@ func hit_by(hitbox):
 					if ticks_left < 0:
 						ticks_left = 0
 #					explode()
+
+
 
 	emit_signal("got_hit")
 
