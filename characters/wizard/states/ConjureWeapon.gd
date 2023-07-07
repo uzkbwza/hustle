bee9b9d531bfbe5416c1194624ce8c042cf5036b
Ian (Ivy) Sly <43023911+uzkbwza@users.noreply.github.com>
2023-07-06 22:10:19 -0700

    1.7.0

--- characters/wizard/states/ConjureWeapon.gd
+++ characters/wizard/states/ConjureWeapon.gd
@@ -8,10 +8,12 @@ func _frame_0():
 	got_hit = false
 	hitbox.hitstun_ticks = 20
 
+func _frame_3():
+	host.start_projectile_invulnerability()
+
 func _frame_6():
 	if !got_hit:
 		host.has_hyper_armor = true
-	host.start_projectile_invulnerability()
 
 func _frame_14():
 	host.has_hyper_armor = false
