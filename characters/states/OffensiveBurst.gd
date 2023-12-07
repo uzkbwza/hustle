27642db53579ade358c4fea3d24e8c1329d38a58
Ian (Ivy) Sly <43023911+uzkbwza@users.noreply.github.com>
2023-12-07 11:16:44 -0800

    1.8.33-unstable

--- characters/states/OffensiveBurst.gd
+++ characters/states/OffensiveBurst.gd
@@ -26,5 +26,6 @@ func is_usable():
 	return host.burst_enabled and (host.hit_fighter_last() or host.combo_count > 0) and (host.bursts_available > 0) and .is_usable()
 
 func _on_hit_something(obj, hitbox):
+	._on_hit_something(obj, hitbox)
 	if obj and obj.is_in_group("Fighter"):
 		host.burst_cancel_combo = true
