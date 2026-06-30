74c7fa12eb0a816b1d4d210e929cb1fd3c08509e
BargunYomi <williamsoscar101107@gmail.com>
2026-06-30 21:07:31 +1000

    Fixed robot loic

--- characters/robo/RoboExtra.gd
+++ characters/robo/RoboExtra.gd
@@ -29,11 +29,13 @@ func get_extra():
 		"armor_enabled": $"%ArmorEnabled".pressed,
 		"nade_activated": $"%NadeActive".pressed and $"%NadeActive".visible,
 		"pull_enabled": $"%PullEnabled".pressed and $"%PullEnabled".visible,
-		"loic_dir": loic.get_data() if loic.is_visible_in_tree() else {"x": fighter.loic_dir, "y": 0},
 		"drive_cancel": drive_pressed() if fighter.stance != "Drive" else !drive_pressed(),
 		"bounce": bounce.get_data(),
 		"honk": $"%HonkEnabled".pressed
 	}
+if loic.is_visible_in_tree():
+		extra["loic_dir"] = loic.get_data()
+	return extra
 
 func drive_pressed():
 	return $"%DriveCancel".pressed and $"%DriveCancel".visible
